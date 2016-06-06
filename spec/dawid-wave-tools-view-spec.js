ad8539628e728810d4ce47eb7def5ebaa7d557be
Dawid Naude <dawidnaude@gmail.com>
2016-06-06 21:18:30 +1000

    Working SAQL Converter

---
+++ spec/dawid-wave-tools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DawidWaveToolsView from '../lib/dawid-wave-tools-view';
+
+describe('DawidWaveToolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
